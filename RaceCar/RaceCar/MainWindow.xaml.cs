2169f749e75165325e575e454ed579cab56c3a3f
btuladhar22 <btuladhar22@my.whitworth.edu>
2020-01-24 01:07:24 -0800

    Done

--- RaceCar/RaceCar/MainWindow.xaml.cs
+++ RaceCar/RaceCar/MainWindow.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿//////////////////////////////////////////////////////////////////////////////////
+// FINAL PROJECT
+// CS371 - Whitworth University
+// Members : Bishesh Tuladhar, Pukar Mahat
+/////////////////////////////////////////////////////////////////////////////
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,72 +24,60 @@ using System.Media;
 
 namespace RaceCar
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+
     public partial class MainWindow : Window
     {
-        string keyPressing="";
         //global variables
-        int carSpeed = 10;
-        double roadSpeed = 5;
-        bool carLeft;
-        bool carRight;
-        double trafficSpeed = 5;
-        int Score = 0;
-        Random rnd = new Random();
-        System.Timers.Timer gTimerFlag = new System.Timers.Timer();
-        DispatcherTimer timer;
 
-        SoundPlayer music = new SoundPlayer();
-        SoundPlayer hit = new SoundPlayer();
+        double carSpeed = 10; //player car speed
+        double roadSpeed = 5; //roadtrack speed
+        double trafficSpeed = 5; //AI car speed
+        int Score = 0; //Score
 
+        bool carLeft; //bool for left move
+        bool carRight; //bool for right move
 
+        Random rnd = new Random(); //random number generator
+        DispatcherTimer timer; //timer 
+
+        SoundPlayer music = new SoundPlayer(); //background music
+        SoundPlayer hit = new SoundPlayer(); //hit music
+
+        string keyPressing = ""; //DEVELOPER STATS of key
 
         public MainWindow() 
         {
             InitializeComponent();
-            
 
-            timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromSeconds(.01);
-            timer.Tick += timer_Tick;
+            timer = new DispatcherTimer(); //initializing the timer 
+            timer.Interval = TimeSpan.FromSeconds(.01); //setting the tick 
+            timer.Tick += timer_Tick; 
 
-            string rootLocation = System.IO.Path.GetFullPath("../../Resources/TokyoDrift.wav");
-            string rootLocation2 = System.IO.Path.GetFullPath("../../Resources/hit.wav");
+            string rootLocation = System.IO.Path.GetFullPath("../../Resources/TokyoDrift.wav"); //root location of the background music
+            string rootLocation2 = System.IO.Path.GetFullPath("../../Resources/hit.wav"); //root location of the hit msuic
             music.SoundLocation = rootLocation;
             hit.SoundLocation = rootLocation2;
         
-            KeyDown += moveCar;
-            KeyUp += stopCar;
+            KeyDown += moveCar; //key down event
+            KeyUp += stopCar; //key up event
 
-            Reset();
+            Reset(); //reseting the game
 
         }
 
 
-        private void OnKeyDownHandler(object sender, KeyEventArgs e)
-        {
-                moveCar(sender,e);
-        }
-
-        private void OnKeyUpHandler(object sender, KeyEventArgs e)
-        {
-            stopCar(sender, e);
-        }
-
+        //Method to reset the game 
         public void Reset()
         {
-            music.Play();
+            music.Play(); //music is played
 
-            trophy.Visibility = Visibility.Hidden;
+            trophy.Visibility = Visibility.Hidden; //trophy is hidden
 
-            explosion.Visibility = Visibility.Hidden;
+            explosion.Visibility = Visibility.Hidden; //explosion is hiden
 
-            trafficSpeed = 5;
-
+            //Setting default value
+            trafficSpeed = 5; 
             roadSpeed = 5;
-
             Score = 0;
 
             
@@ -114,34 +108,47 @@ namespace RaceCar
             Canvas.SetLeft(AI2Hit, 59);
             Canvas.SetTop(AI2Hit, 78);
 
-            roadTrack1.Margin = new Thickness(-2,-638, 0, 0);
-            roadTrack2.Margin = new Thickness(-3, -222, 0, 0);
+            //Reseting the road track
+            Canvas.SetLeft(roadTrack1, -3);
+            Canvas.SetTop(roadTrack1, -627);
+
+            Canvas.SetLeft(roadTrack2, -5);
+            Canvas.SetTop(roadTrack2, -208);
 
             //Reseting Explosion
             Canvas.SetLeft(explosion, 119);
             Canvas.SetTop(explosion, 240);
 
+            //beginning the timeer
             timer.Start();
 
         }
 
+        //method to check if two rectangel collide with each other
         public bool intersectsWith(Rectangle ob1, Rectangle ob2)
         {
-            Rect myRect1 = new Rect();
+            Rect myRect1 = new Rect(); //genrating new rect ob for 1st rectangle
 
-            double top1 = Canvas.GetTop(ob1);
+            //Setting the rect with same co-ordinate as the rectangle
+            double top1 = Canvas.GetTop(ob1); 
             double left1 = Canvas.GetLeft(ob1);
 
-            myRect1.Location = new Point(left1, top1);
+            //Setting the rect with same location and size as the rectangle
+            myRect1.Location = new Point(left1, top1); 
             myRect1.Size = new Size(ob1.Width, ob1.Height);
 
-            Rect myRect2 = new Rect();
+            Rect myRect2 = new Rect(); //genrating new rect ob for 1st rectangle
+
+            //Setting the rect with same co-ordinate as the rectangle
             double top2 = Canvas.GetTop(ob2);
             double left2 = Canvas.GetLeft(ob2);
 
+            //Setting the rect with same location and size as the rectangle
             myRect2.Location = new Point(left2, top2);
             myRect2.Size = new Size(ob2.Width, ob2.Height);
 
+
+            //Checking if the two rect with each other
             if (myRect1.IntersectsWith(myRect2))
             {
                 return true;
@@ -152,82 +159,29 @@ namespace RaceCar
             }
         }
 
-        private bool isOn(Image img1, Image img2)
-        {
-            if (img1 == null || img1.Visibility != System.Windows.Visibility.Visible)
-            {
-                return false;
-            }
-
-            double img1_topLeft_X = img1.Margin.Left;
-            double img1_topLeft_Y = img1.Margin.Top;
-            double img1_bottomRight_X = img1_topLeft_X + img1.Width;
-            double img1_bottomRight_Y = img1_topLeft_Y + img1.Height;
-
-            Point img1_topLeft = new Point(img1_topLeft_X, img1_topLeft_Y);
-            Point img1_bottomRight = new Point(img1_bottomRight_X, img1_bottomRight_Y);
-
-            HitTestResult result_topLeft = VisualTreeHelper.HitTest(img2.Parent as Canvas, img1_topLeft);
-            HitTestResult result_bottomRight = VisualTreeHelper.HitTest(img2.Parent as Canvas, img1_bottomRight);
-
-            if (result_topLeft != null && result_bottomRight != null)
-            {
-                if (result_topLeft.VisualHit.GetType() == typeof(Image) && result_bottomRight.VisualHit.GetType() == typeof(Image) &&
-                    (result_topLeft.VisualHit as Image).Name.Equals(img2.Name) && (result_bottomRight.VisualHit as Image).Name.Equals(img2.Name))
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            else
-            {
-                return false;
-            }
-
-        }
-
+        //timer_Tick method to circle thorugh each tick
         void timer_Tick(object sender, EventArgs e)
         {
             Score++;
 
             distance.Content = "" + Score;
 
-            Thickness r1 = roadTrack1.Margin;
-            r1.Top += roadSpeed;
+            //Movement of the roadtrack
+            Canvas.SetTop(roadTrack1, Canvas.GetTop(roadTrack1) + roadSpeed);
+            Canvas.SetTop(roadTrack2, Canvas.GetTop(roadTrack2) + roadSpeed);
 
-            Thickness r2 = roadTrack2.Margin;
-            r2.Top += roadSpeed;
-
-            roadTrack1.Margin = r1;
-            roadTrack2.Margin = r2;
-
-            Stats.Text = "R1 = " + Convert.ToString(roadTrack1.Margin) + " \n R2 = " + Convert.ToString(roadTrack2.Margin) +
-                                "\n C1 = " + Convert.ToString(Canvas.GetLeft(AI1)) + " , " + Convert.ToString(Canvas.GetTop(AI1)) + 
-                                "\n C2 = " + Convert.ToString(Canvas.GetLeft(AI2)) + " , " + Convert.ToString(Canvas.GetTop(AI2)) +
-                                "\n PLayer = " + Convert.ToString(Canvas.GetLeft(player)) + " , " + Convert.ToString(Canvas.GetTop(player)) +
-                                "\n Explosion = " + Convert.ToString(Canvas.GetLeft(explosion)) + " , " + Convert.ToString(Canvas.GetTop(explosion)) +
-                                "\n PlayerHIt = " + Convert.ToString(Canvas.GetLeft(playerHit)) + " , " + Convert.ToString(Canvas.GetTop(playerHit)) +
-                                "\n AI1hit = " + Convert.ToString(Canvas.GetLeft(AI1Hit)) + " , " + Convert.ToString(Canvas.GetTop(AI1Hit)) +
-                                "\n AI2hit = " + Convert.ToString(Canvas.GetLeft(AI2Hit)) + " , " + Convert.ToString(Canvas.GetTop(AI2Hit))+
-                                "\n Key Pressing " + keyPressing;
-
-
-
-            if (roadTrack1.Margin.Top > 400)
+            //Resetting the road track if it goes out of bound
+            if (Canvas.GetTop(roadTrack1) > 500)
             {
-                r1.Top = -630;
-                roadTrack1.Margin = r1;
+                Canvas.SetTop(roadTrack1, -630);
             }
 
-            if (roadTrack2.Margin.Top > 400)
+            if (Canvas.GetTop(roadTrack2) > 500)
             {
-                r2.Top = -630;
-                roadTrack2.Margin = r2;
+                Canvas.SetTop(roadTrack2, -630);
             }
 
+            //checking if the left is still within bound
             if (Canvas.GetLeft(player) < 0 || Canvas.GetLeft(playerHit) < 0)
             {
                 carLeft = false;
@@ -242,7 +196,7 @@ namespace RaceCar
                 Canvas.SetLeft(playerHit, Canvas.GetLeft(playerHit) - carSpeed);
             }
 
-   
+            //checking if the left is still within bound
             if (Canvas.GetLeft(player) > 334 || Canvas.GetLeft(playerHit) > 334)
             {
                 carRight = false;
@@ -266,33 +220,33 @@ namespace RaceCar
             Canvas.SetTop(AI2, Canvas.GetTop(AI2) + trafficSpeed);
             Canvas.SetTop(AI2Hit, Canvas.GetTop(AI2Hit) + trafficSpeed);
 
-            //Respawing the AI cars and changing their images
+            //Respawing the AI1 cars and changing their images
             if (Canvas.GetTop(AI1) > Base.Height)
             {
                 changeAI1();
 
 
-                double newrand = rnd.Next(0, 160);
+                double newrand = rnd.Next(0, 160); //generating new random for the AI position 
 
                 Canvas.SetLeft(AI1, newrand);
                 Canvas.SetLeft(AI1Hit, newrand);
 
-                double randTop = rnd.Next(400, 400) * -1;
+                double randTop = rnd.Next(400, 400) * -1; //generating new random for the AI position 
 
                 Canvas.SetTop(AI1, randTop);
                 Canvas.SetTop(AI1Hit, randTop);
             }
 
+            //Respawing the AI1 cars and changing their images
             if (Canvas.GetTop(AI2) > Base.Height)
             {
-                changeAI2();
+                changeAI2(); //changing the image
 
-
-                double newrand = rnd.Next(161, 300);
+                double newrand = rnd.Next(161, 300); //generating new random for the AI position 
                 Canvas.SetLeft(AI2, newrand);
                 Canvas.SetLeft(AI2Hit, newrand);
 
-                double randTop = rnd.Next(400, 600) * -1;
+                double randTop = rnd.Next(400, 600) * -1; //generating new random for the AI position 
 
                 Canvas.SetTop(AI2, randTop);
                 Canvas.SetTop(AI2Hit, randTop);
@@ -309,58 +263,54 @@ namespace RaceCar
             }
 
             //Speed up the traffic 
+            //Compund increase after every cycle
             trafficSpeed = trafficSpeed * 1.0001;
             roadSpeed = roadSpeed * 1.0001;
-            /*
-            if (Score > 100 && Score < 500)
-            {
-                trafficSpeed = 6;
-                roadSpeed = 7;
-            }
-            else if (Score >= 500 && Score < 1000)
-            {
-                trafficSpeed = 7;
-                roadSpeed = 8;
-            }
-            else if (Score > 1200)
-            {
-                trafficSpeed = 9;
-                roadSpeed = 10;
-            }*/
 
         }
 
+        //moveCar method
         private void moveCar(object sender, KeyEventArgs e)
         {
-            keyPressing = e.Key.ToString();
+            keyPressing = e.Key.ToString(); //DEVELOPER Stats 
+
+            //if the key pressed is 'A' and if the player is within bound move left
             if (e.Key.ToString() == "A" && Canvas.GetLeft(player) > 0)
             {
                 carLeft = true;
             }
 
+            //if the key pressed is 'D' and if the player is within bound move right
             if (e.Key.ToString() == "D" && Canvas.GetLeft(player) < 334)
             {
                 carRight = true;
             }
         }
+        
 
+        //stopCar method
         private void stopCar(object sender, KeyEventArgs e)
         {
-            if (e.Key.ToString() == "A")
+            if (e.Key.ToString() == "A") //stops the car movement to left after key is up
             {
                 carLeft = false;
                 
             }
-            if (e.Key.ToString() == "D")
+            if (e.Key.ToString() == "D") //stops the car movement to right after key is up
             {
                 carRight = false;
             }
-            keyPressing = "";
+
+            keyPressing = ""; //DEVELOPER stats
         }
 
+        //changeAI1 function
+        //changes the car images
         private void changeAI1()
         {
-            int num = rnd.Next(1, 8);
+            int num = rnd.Next(1, 8); //generating random number from 1 -8
+
+            //Selects a car type according to the random number selection
             switch (num)
             {
 
@@ -410,7 +360,9 @@ namespace RaceCar
 
         private void changeAI2()
         {
-            int num = rnd.Next(1, 8);
+            int num = rnd.Next(1, 8); //generating random number from 1 -8
+
+            //Selects a car type according to the random number selection
             switch (num)
             {
 
@@ -458,21 +410,25 @@ namespace RaceCar
             }
         }
 
+
+        //gameOver function
         private void gameOver()
         {
-            music.Stop();
+            music.Stop(); //music stops
 
-            trophy.Visibility = Visibility.Visible;
+            trophy.Visibility = Visibility.Visible; //trophy is displayed
 
-            timer.Stop();
+            timer.Stop(); //cycle is stoped
 
 
-            explosion.Visibility = Visibility.Visible;
+            explosion.Visibility = Visibility.Visible; //explosion of the car
 
+            //setting the explosion on top of the player 
             Canvas.SetLeft(explosion, Canvas.GetLeft(player)-45);
             Canvas.SetTop(explosion, Canvas.GetTop(player)-20);
 
 
+            //Selecting the trophy according to the score
             if (Score < 100)
             {
                 trophy.Source = new BitmapImage(new Uri("bronze.png", UriKind.Relative));
@@ -488,12 +444,31 @@ namespace RaceCar
                 trophy.Source = new BitmapImage(new Uri("gold.png", UriKind.Relative));
             }
 
-            hit.Play();
+            hit.Play();//hit music
+
         }
 
+        //Button to reset
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             Reset();
+        }
+
+        //DEVELOPER STATS TEXTBOX
+        private void DevelopStats()
+        {
+            Stats.Visibility = Visibility.Visible; //visible the textbox once it is called
+
+            Stats.Text = "R1 = " + Convert.ToString(Canvas.GetLeft(roadTrack1)) + " , " + Convert.ToString(Canvas.GetTop(roadTrack1)) +
+                                " \n R2 = " + Convert.ToString(Canvas.GetLeft(roadTrack2)) + " , " + Convert.ToString(Canvas.GetTop(roadTrack2)) +
+                                "\n C1 = " + Convert.ToString(Canvas.GetLeft(AI1)) + " , " + Convert.ToString(Canvas.GetTop(AI1)) +
+                                "\n C2 = " + Convert.ToString(Canvas.GetLeft(AI2)) + " , " + Convert.ToString(Canvas.GetTop(AI2)) +
+                                "\n PLayer = " + Convert.ToString(Canvas.GetLeft(player)) + " , " + Convert.ToString(Canvas.GetTop(player)) +
+                                "\n Explosion = " + Convert.ToString(Canvas.GetLeft(explosion)) + " , " + Convert.ToString(Canvas.GetTop(explosion)) +
+                                "\n PlayerHIt = " + Convert.ToString(Canvas.GetLeft(playerHit)) + " , " + Convert.ToString(Canvas.GetTop(playerHit)) +
+                                "\n AI1hit = " + Convert.ToString(Canvas.GetLeft(AI1Hit)) + " , " + Convert.ToString(Canvas.GetTop(AI1Hit)) +
+                                "\n AI2hit = " + Convert.ToString(Canvas.GetLeft(AI2Hit)) + " , " + Convert.ToString(Canvas.GetTop(AI2Hit)) +
+                                "\n Key Pressing " + keyPressing;
         }
 
 
